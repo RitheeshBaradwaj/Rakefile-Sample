0db58da7cf64ba5e66937bb98dff6564fe25cc35
Ritheesh <bunnyrb4@gmail.com>
2020-05-11 12:53:59 +0530

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,46 @@
+﻿clear;
+
+$arr = "      HELLO FRIEND","      THIS IS MY","      WORKSPACE","      ......","      Welcome! :)","      Let's Get Started!!";
+
+
+foreach ($i in $arr){
+    
+    Write-Host ""
+    Write-Host "$i"
+    Start-Sleep -s 1;
+    clear;
+
+}
+
+clear;
+Write-Host  "          Hello, Brother! Welcome to my WorkSpace!!`n";
+Write-Host -ForegroundColor DarkMagenta "          My Profile`n";
+Write-Host -ForegroundColor Green "          http://ritheeshbaradwaj.github.io/";
+Start-Sleep -s 5;
+
+clear;
+Write-Host  "          My Projects`n";
+Write-Host -ForegroundColor DarkMagenta "          Meme Generator using AI`n";
+Write-Host -ForegroundColor Green "          https://developers-cosmos.github.io/Meme-Generator/";
+Start-Sleep -s 5;
+
+clear;
+Write-Host  "          My Projects`n";
+Write-Host -ForegroundColor DarkMagenta "          CI/CD Pipeline`n";
+Write-Host -ForegroundColor Green "          https://ritheeshbaradwaj.github.io/JenkinsPipeline/";
+Start-Sleep -s 5;
+
+clear;
+Write-Host  "          My Projects`n";
+Write-Host -ForegroundColor DarkMagenta "          Configuation Management - Chef`n";
+Write-Host -ForegroundColor Green "          https://developers-cosmos.github.io/COVID19-FeedbackApplication/";
+Start-Sleep -s 5;
+
+clear;
+Write-Host  "          My Projects`n";
+Write-Host -ForegroundColor DarkMagenta "          Git Learn - A tool to learn Git`n";
+Write-Host -ForegroundColor Green "          https://ritheeshbaradwaj.github.io/GitLearn/";
+Start-Sleep -s 5;
+
+clear;
+Write-Host -ForegroundColor Red "`nEND OF THE SLIDE";
