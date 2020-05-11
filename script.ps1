46605020ea10de4b1e13c20b23999d46bfe819ba
Ritheesh <bunnyrb4@gmail.com>
2020-05-11 13:39:21 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -15,28 +15,28 @@ foreach ($i in $arr){
 clear;
 Write-Host  "`n          Hello, Brother! Welcome to my WorkSpace!!`n";
 Write-Host -ForegroundColor DarkMagenta "          My Profile`n";
-Write-Host -ForegroundColor Green "          http://ritheeshbaradwaj.github.io/";
+Write-Host -ForegroundColor Green "          http://ritheeshbaradwaj.github.io/`n";
 Start-Sleep -s 5;
 
 clear;
 Write-Host  "`n          My Projects`n";
 Write-Host -ForegroundColor DarkMagenta "          Meme Generator using AI`n";
-Write-Host -ForegroundColor Green "          https://developers-cosmos.github.io/Meme-Generator/";
+Write-Host -ForegroundColor Green "          https://developers-cosmos.github.io/Meme-Generator/`n";
 Start-Sleep -s 5;
 
 clear;
 Write-Host -ForegroundColor DarkMagenta "`n          CI/CD Pipeline`n";
-Write-Host -ForegroundColor Green "          https://ritheeshbaradwaj.github.io/JenkinsPipeline/";
+Write-Host -ForegroundColor Green "          https://ritheeshbaradwaj.github.io/JenkinsPipeline/`n";
 Start-Sleep -s 5;
 
 clear;
 Write-Host -ForegroundColor DarkMagenta "`n          Configuation Management - Chef`n";
-Write-Host -ForegroundColor Green "          https://developers-cosmos.github.io/COVID19-FeedbackApplication/";
+Write-Host -ForegroundColor Green "          https://developers-cosmos.github.io/COVID19-FeedbackApplication/`n";
 Start-Sleep -s 5;
 
 clear;
 Write-Host -ForegroundColor DarkMagenta "`n          Git Learn - A tool to learn Git`n";
-Write-Host -ForegroundColor Green "          https://ritheeshbaradwaj.github.io/GitLearn/";
+Write-Host -ForegroundColor Green "          https://ritheeshbaradwaj.github.io/GitLearn/`n";
 Start-Sleep -s 5;
 
 clear;
